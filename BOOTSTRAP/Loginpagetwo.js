b80dfb0636460e6fe27d1e7cc49a7b9f3bcf9fad
psalmmy10 <samueladesanya10@gmail.com>
2022-04-23 11:23:16 +0100

    master

--- BOOTSTRAP/Loginpagetwo.js
+++ BOOTSTRAP/Loginpagetwo.js
@@ -1,6 +1,5 @@
 function myFunction() {
     setTimeout(() => {
       window.location.assign("file:///C:/Users/DELL/Desktop/My%20Project/BOOTSTRAP/Loginpagethree.html")
-    }, 2000);
-    
+    }, 2000); 
   }
