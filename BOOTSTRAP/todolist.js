27f3ef6cfb9b383a84dfb033e1bea08f57a6b77b
psalmmy10 <samueladesanya10@gmail.com>
2022-04-27 12:01:09 +0100

    master

--- BOOTSTRAP/todolist.js
+++ BOOTSTRAP/todolist.js
@@ -2,70 +2,71 @@ var arr = [];
 var table = document.getElementById('table');
 let editIndex ="";
 var selectDropDown = document.getElementById("opt")
+var selectDropDown2 = document.getElementById("opt1")
 var select =[
                {name:"Bread", price:10},
                {name:"Bread and Beans", price : 20}, 
                {name:"Rice", price:40} ,
                {name:"Yam", price: 50}, 
-               {name:"Egg", price:60}
+               {name:"Egg", price:60},
+               {name:"Sugar", price:70}
             ];
-
 
 for (let i = 0 ; i < select.length ; i++){
     selectDropDown.innerHTML+=`<option value="${select[i].name}">${select[i].name}</option>`
+    selectDropDown2.innerHTML+=`<option value="${select[i].item}, ${select[i].price}">${select[i].name}</option>`
 }
 
 
 
 
 
- 
-function getItemPrice(e){
-  var priceOutput = "";
-  var getPrice = e.target.value;
-  var getInputValue =document.getElementById("price")
-  for(let i = 0 ; i < select.length ; i++){
-      if (select[i].name == getPrice){
-        priceOutput = select[i].price
-      }
-  }
-  getInputValue.value = priceOutput;
-  fetchData()
-}
 
 
-
-
+//PUSH FUNCTION
 function myList(){
+    var status = false
     var item = document.getElementById('opt').value;
     var amount = document.getElementById('price').value;
+  
+    
     var tod = {
         item , amount:amount
     }
-    if(item  ==  "" || amount == " "){
+
+    for(let i = 0; i<arr.length ; i++){
+        if(item == arr[i].item){
+           status = true
+        }
+    }
+
+
+    if(item  ==  "" || amount == ""){
         alert('Add an Item')
     }
-    else{
+    else if(status){
+        alert ("Item already selected")
+    }
+    else {
         arr.push(tod);
         fetchData()
     }
-    // console.log(amount);
 }
 
-//Add Function
+//SUM UP FUNCTION
 function fetchData(){
-    table.innerHTML= '';
-    var totalPrice =  Number(); 
+    table.innerHTML='';
+    var totalPrice = Number(); 
     for(let i=0; i<arr.length; i++){
-        totalPrice += Number(arr[i].amount) 
+        totalPrice +=  Number(arr[i].amount) 
         table.innerHTML+= `
         <tr>
             <td>${i+1}</td>
             <td >${arr[i].item}</td>
-            <td id="sumUp">${"$" + arr[i].amount}</td>
+            <td id="">${"$" + arr[i].amount}</td>
             <td> 
                  <i class="fa-solid fa-trash" onclick="deletetodo(${i})"></i>
-                 <i class="fa-solid fa-pen" onclick="editTodo(${i})" ></i>
+                 <i class="fa-solid fa-pen" onclick="editTodo(${i})" data-toggle="modal" data-target="#exampleModal" ></i>
             </td>
         </tr>
         ` 
@@ -74,41 +75,52 @@ function fetchData(){
     document.getElementById("cal").value = totalPrice;
 }
 
+//PRICE FUNCTION
+function getItemPrice(e){
+  var priceOutput ="";
+  var getPrice = e.target.value;
+  var getInputValue = document.getElementById("price")
+    for(let i = 0 ; i < select.length ; i++){
+        if (select[i].name == getPrice){
+          priceOutput = select[i].price
+        }
+    }
+    getInputValue.value = priceOutput;
+    fetchData()
+    console.log(getPrice);
+}
 
+//DELETE FUNCTION
 function deletetodo(ind){
     arr.splice(ind,1)
     fetchData()
 }
 
-// Edit update
-
+// UPDATE FUNCTION
 function update(){
-    let inputValue = document.getElementById("todo");
+    let inputValue = document.getElementById("price1");
     for (let i =0; i<arr.length; i++){
         if(i == editIndex){
             arr[i].item = inputValue
+            arr[i].amount = inputValue
         }
+        console.log(price1,opt1);
     }
     document.getElementById("list").classList.remove("display")
-    document.getElementById("update").classList.add("display")
+    // document.getElementById("update").classList.add("display")
     fetchData()
 }
 
-// Edit Function
-
+// EDIT FUNCTION // MODAL POP-UP && ITEM SELECTION
 function editTodo(edit){
     editIndex = edit
-    let valued ="";
-    let input = document.getElementById("todo");
-    document.getElementById("list").classList.add("display")
-    document.getElementById("update").classList.remove("display")
-
-    arr.forEach ((item, i) =>{
-        if (i == edit){
-            valued = item
-        }
-    })
-    fetchData()
 };
+
+
+
+
+
+
+
 
 
