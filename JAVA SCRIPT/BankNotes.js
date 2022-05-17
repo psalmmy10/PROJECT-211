645aeeee177b54d7e4cd5d9f7f25e83300867a86
psalmmy10 <samueladesanya10@gmail.com>
2022-05-17 14:26:59 +0100

    master

--- JAVA SCRIPT/BankNotes.js
+++ JAVA SCRIPT/BankNotes.js
@@ -1,3 +1,24 @@
+var input1 = document.querySelector("#Note1").value;
+// var input2 = document.querySelector("#Note2")
+// var input3 = document.querySelector("#Note3")
+
+
 function bankNotes(){
-   
+
+    quotient = 1000;     
+    remainder = 1000; 
+
+    if(input1.value == typeof(Number)){
+        input1.value / quotient 
+        input1.value / remainder 
+        
+        division =  input1.value / quotient 
+
+        output.innerHTML = division
+        // quotient1 = remainder/ 100;     
+        // remainder1 = remainder % 100;    
+
+        // quotient2 = remainder1 / 10;     
+    }
+    console.log(input1.value);
 }
