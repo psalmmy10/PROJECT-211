4443a5902b6fe5c4ca6974525c6a215fe08dd353
psalmmy10 <samueladesanya10@gmail.com>
2022-06-01 07:27:16 +0100

    master

--- MyQuizApp/MyQuizAppDashBoard.js
+++ MyQuizApp/MyQuizAppDashBoard.js
@@ -4,3 +4,24 @@ $(document).ready(function () {
     });
 });
 
+function fetchCategory(){
+    let categoryData = JSON.parse(localStorage.getItem("cat"))
+    if(categoryData != null){
+        var i = 1;
+        categoryData.forEach((element, a) =>{
+           
+            
+         });
+         document.querySelector(".count").innerHTML = categoryData.length
+    }
+   
+}
+fetchCategory()
+
+let auth = JSON.parse(localStorage.getItem("authUser"))
+if(auth){
+    intro.innerHTML = `Welcome` +" "+ auth.uname
+}else{
+    window.location.assign("file:///C:/Users/DELL/Desktop/My%20Project/MyQuizApp/MyQuizAppDash.html")
+}
+
