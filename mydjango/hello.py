469316ce7334257e928fb764810de2c1706c3430
Psalmuel <66147996+psalmmy10@users.noreply.github.com>
2024-04-13 13:16:44 +0000

    aster

---
+++ mydjango/hello.py
@@ -0,0 +1,12 @@
+def my_function(num,num2):
+  return num%num2
+
+num = input('pick a number ')
+num2 = int(input('pick a number '))
+
+
+
+for i in num:
+  print(i)
+  
+  # print(my_function(num,num2))
